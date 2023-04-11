d6c669c8f182df8628f04a6b88d221416df1c59c
Melanieislas <120453099+Melanieislas@users.noreply.github.com>
2023-04-10 22:47:18 -0700

    added save and append functionality to individual notes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const uuid = require('./helpers/uuid');
 var fs = require('fs');
 const dbJson = require('./db/db.json');
+const util = require('util');
+
 
 const app = express();
 const PORT = 3001;
@@ -19,10 +21,47 @@ app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+const readFromFile = util.promisify(fs.readFile);
+
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for a new note`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+/*
 app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received for a new note`);
     res.status(200).json(dbJson);
-});
+});*/
 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
@@ -35,6 +74,13 @@ app.post('/api/notes', (req, res) => {
             text,
             note_id: uuid(),
         };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added`);
+    } else {
+        res.errored('Error');
+    }
+        /*
 
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
@@ -65,7 +111,8 @@ app.post('/api/notes', (req, res) => {
     } else {
         res.status(500).json('Error in adding note');
 
-    }
+    }*/
+
 });
 
 app.listen(PORT, () =>
